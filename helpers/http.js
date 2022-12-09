7e49e5c080e6f47b2d4e49aacb9fd71776456126
znareak <libardojesusrengifo129@gmail.com>
2022-12-09 16:04:27 +0100

    change source data api

--- helpers/http.js
+++ helpers/http.js
@@ -2,24 +2,24 @@ const axios = require("axios").default;
 const { JSDOM } = require("jsdom");
 
 async function getPrices() {
-  const url = "https://www.bcv.org.ve/";
+  const url = "https://monitordolarvenezuela.com/";
   try {
-    const htmltext = await axios.get(url);
-    const dom = new JSDOM(htmltext.data);
-    const find = (id) => {
-      const data = dom.window.document.querySelector(`#${id} .centrado strong`);
-      const value = data.innerHTML.replace(",", ".");
-      return Number(value).toFixed(2);
-    };
-    const dollar = find("dolar");
-    const euro = find("euro");
-    const date = dom.window.document.querySelector(".dinpro span").innerHTML;
-    return {
-      origin: "http://www.bcv.org.ve/",
-      date,
-      dollar,
-      euro,
-    };
+    const htmltext = await axios.get(url, {
+      responseType: "text",
+      headers: { "Accept-Encoding": "gzip,deflate,compress" }
+    });
+    const dom = new JSDOM(htmltext.data.replaceAll("\n", ""));
+
+    const findContent = (path, prop = "textContent") => {
+      const data = dom.window.document.querySelector("#promedios .row div:nth-of-type(2) > div > " + path)[prop]
+      return data;
+    }
+
+    const dollar = findContent("p").match(/(\d+)(?:\,(\d{1,2}))?/)[0]
+    const date = findContent("small:nth-of-type(2)", "innerHTML").replace("<br>", "  ")
+
+    console.log({ dollar })
+    return { dollar, date, url }
   } catch (e) {
     console.error(e.message);
     throw new Error(e.message);
